f0f211a79bf995756b419231218b210be17e47fd
pepperjackdev <giacomodeflorio9093@gmail.com>
2025-08-16 18:54:58 +0200

    Added tilepp dependency

--- src/platformer/main.cpp
+++ src/platformer/main.cpp
@@ -1,19 +1,14 @@
 #include <raylib.h>
-#include "tiled.hh"
 
 int main() {
-    // SetTargetFPS(60);
-    // SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    // InitWindow(900, 900, "Platformer");
+    SetTargetFPS(60);
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(900, 900, "Platformer");
 
-    Map map = Map("assets/maps/level.json");
-    Layer layer = *map.getLayers().begin();
-
-    // while (!WindowShouldClose()) {
-    //     BeginDrawing();
-    //         ClearBackground(RAYWHITE);
-    //     EndDrawing();
-    // }
-    // 
-    // CloseWindow();
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+    CloseWindow();
 }

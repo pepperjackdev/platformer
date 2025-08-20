35b39b4e32cd728a51204c7dccef4990a70122b2
pepperjackdev <giacomodeflorio9093@gmail.com>
2025-08-20 22:17:54 +0200

    Created Player and Blocl

--- src/platformer/main.cpp
+++ src/platformer/main.cpp
@@ -1,104 +1,127 @@
 #include <raylib.h>
-#include <raymath.h>
 
-#include <list>
 #include <memory>
+#include <vector>
 
 class Node {
-    private:
-        std::list<std::unique_ptr<Node>> children;
-    public:
-        void addChild(std::unique_ptr<Node> node) {
-            this->children.push_back(std::move(node));
-        }
-
-        virtual void update() {
-            for (std::unique_ptr<Node> &child: this->children) {
-                child.get()->update();
-            }
-        }
-
-        virtual void draw() {
-            for (std::unique_ptr<Node> &child: this->children) {
-                child.get()->draw();
-            }
-        }
-
-};
-
-class PhysicNode:
-    public Node {
-
-    private:
-        Vector2 position;
-        Vector2 momentum;
-        float mass;
+    protected:
+        Rectangle rectangle;
         Texture2D texture;
 
     public:
-        PhysicNode(Vector2 position, Vector2 momentum, float mass, Texture2D texture): 
-            position(position), momentum(momentum), mass(mass), texture(texture) {
-            }
+        Node(Rectangle rectangle, Texture2D texture):
+            rectangle(rectangle), texture(texture) {}
 
-        Vector2 getPosition() {
-            return this->position;
+        Rectangle& getRectangle() {
+            return this->rectangle;
         }
 
-        Vector2 getMomentum() {
-            return this->momentum;
+        Texture2D& getTexture() {
+            return this->texture;
         }
 
-        Vector2 getVelocity() {
-            return Vector2Scale(this->momentum, 1.0f / this->mass);
-        }
+        // Override me!
+        virtual void update() {}
 
-        int getMass() {
-            return this->mass;
-        }
-
-        void update() override {
-            this->position = Vector2Add(
-                this->position,
-                Vector2Scale(
-                    this->getVelocity(),
-                    GetFrameTime()
-                )
-            );
-        }
-
-        void draw() override {
+        void draw() {
             DrawTexture(
-                texture, 
-                this->position.x, 
-                this->position.y, 
+                this->texture,
+                this->rectangle.x,
+                this->rectangle.y,
                 WHITE
             );
         }
 };
+
+class Block:
+    public Node {
+    
+    public:
+        Block(Rectangle rectangle, Texture2D texture): 
+            Node(rectangle, texture) {}
+};
+
+class Player:
+    public Node {
+    
+    protected:
+        Camera2D camera;
+    
+    public:
+        Player(Rectangle rectangle, Texture2D texture): 
+            Node(rectangle, texture) {
+                this->camera = (Camera2D){
+                    (Vector2){GetScreenWidth() / 2, GetScreenHeight() / 2},
+                    (Vector2){rectangle.x, rectangle.y},
+                    0,
+                    4.0f
+                };
+            }
+
+        Camera2D& getCamera() {
+            return this->camera;
+        }
+
+        void update() override {
+            // Updating position
+            if (IsKeyDown(KEY_D)) {
+                this->rectangle.x += 1;
+            }
+            
+            if (IsKeyDown(KEY_A)) {
+                this->rectangle.x -= 1;
+            }
+            
+            // Updating camera
+            this->camera.target = (Vector2){
+                this->rectangle.x + this->rectangle.width / 2, 
+                this->rectangle.y + this->rectangle.height / 2
+            };
+        }
+};
+
 
 int main() {
     SetTargetFPS(60);
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(900, 900, "Platformer");
 
-    Texture2D texture = LoadTexture("assets/block.png");
-    Node scene = Node();
-    scene.addChild(std::make_unique<PhysicNode>(
-        (Vector2){10, 10},
-        (Vector2){1000, 1000},
-        1,
-        texture
+    Texture2D blockTexture = LoadTexture("assets/tiles/block.png");
+    Texture2D playerTexture = LoadTexture("assets/tiles/player.png");
+
+    std::vector<std::shared_ptr<Node>> nodes{};
+    nodes.push_back(std::make_shared<Block>(
+        (Rectangle){100, 100, 32, 32},
+        blockTexture
     ));
 
+    auto player = std::make_shared<Player>(
+        (Rectangle){100, 100, 32, 32},
+        playerTexture
+    );
+
+    nodes.push_back(player);
+
     while (!WindowShouldClose()) {
+
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            // Drawing
-            scene.draw();
+            BeginMode2D(player->getCamera());
 
-            // Updating
-            scene.update();
+                // Drawing nodes
+                for (std::shared_ptr node: nodes) {
+                    node->draw();
+                }
+
+                // Updating nodes
+                for (std::shared_ptr node: nodes) {
+                    node->update();
+                }
+
+            EndMode2D();
+
         EndDrawing();
     }
+
     CloseWindow();
 }

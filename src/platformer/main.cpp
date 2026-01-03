4a66a7b64df74a7debcb54a8325309eb430c0279
pepperjackdev <giacomodeflorio9093@gmail.com>
2026-01-03 21:14:22 +0100

    Added side oriented collision detection system

--- src/platformer/main.cpp
+++ src/platformer/main.cpp
@@ -2,30 +2,74 @@
 #include <raymath.h>
 
 #include <memory>
+#include <cstdint>
 
 #include <list>
+#include <optional>
+
+enum class Collision: uint8_t {
+    RIGHTSIDE=0b11111011,
+    LEFTSIDE=0b11110111,
+    UPSIDE=0b01111111,
+    DOWNSIDE=0b10111111,
+};
+
+uint8_t inline CheckCollisionAlpha(Rectangle rec0, Rectangle rec1) {
+    return rec0.y + rec0.height <= rec1.y;
+};
+
+uint8_t inline CheckCollisionBeta(Rectangle rec0, Rectangle rec1) {
+    return rec0.y >= rec1.y + rec1.height;
+};
+
+uint8_t inline CheckCollisionGamma(Rectangle rec0, Rectangle rec1) {
+    return rec0.x + rec0.width >= rec1.x;
+}
+
+uint8_t inline CheckCollisionDelta(Rectangle rec0, Rectangle rec1) {
+    return rec0.x <= rec1.x + rec1.width;
+}
+
+uint8_t inline CheckCollision(Rectangle rec0, Rectangle rec1) {
+    return 0;
+}
+
+std::optional<Collision> CheckSideCollision(Rectangle rec0a, Rectangle rec0b, Rectangle rec1a, Rectangle rec1b) {
+    if (CheckCollisionAlpha(rec0b, rec1b) && CheckCollisionBeta(rec0b, rec1b) &&
+            !CheckCollisionGamma(rec0a, rec1a) && CheckCollisionGamma(rec0b, rec1b)) {
+        return std::optional(Collision::RIGHTSIDE);
+    } else if (CheckCollisionAlpha(rec0b, rec1b) && CheckCollisionBeta(rec0b, rec1b) &&
+            !CheckCollisionDelta(rec0a, rec1a) && CheckCollisionDelta(rec0b, rec1b)) {
+        return std::optional(Collision::LEFTSIDE);
+    } else if (CheckCollisionGamma(rec0b, rec1b) && CheckCollisionDelta(rec0b, rec1b) &&
+            !CheckCollisionAlpha(rec0a, rec1a) && CheckCollisionAlpha(rec0b, rec1b)) {
+        return std::optional(Collision::UPSIDE);
+    } else if (CheckCollisionGamma(rec0b, rec1b) && CheckCollisionDelta(rec0b, rec1b) &&
+            !CheckCollisionBeta(rec0a, rec1a) && CheckCollisionBeta(rec0b, rec1b)) {
+        return std::optional(Collision::DOWNSIDE);
+    } else {
+        std::nullopt;
+    }
+};
 
 class Node {
     protected:
-        Rectangle rectangle;
+        Vector2 position;
         Texture2D texture;
 
     public:
         Node(
-            Rectangle rectangle, 
+            Vector2 position, 
             Texture2D texture): 
-                rectangle(rectangle), 
+                position(position), 
                 texture(texture) {};
 
-        virtual void update(std::list<std::shared_ptr<Node>> nodes) {
-            
-        }
+        virtual void update(std::list<std::shared_ptr<Node>> nodes) {}
 
         void draw() {
-            DrawTexture(
+            DrawTextureV(
                 this->texture, 
-                this->rectangle.x, 
-                this->rectangle.y, 
+                this->position,
                 RAYWHITE
             );
         }
@@ -34,9 +78,9 @@ class Node {
 class Platform: public Node {
     public:
         Platform(
-            Rectangle rectangle, 
+            Vector2 position, 
             Texture2D texture):
-                Node(rectangle, texture) {};
+                Node(position, texture) {};
 };
 
 class Entity: public Node {
@@ -46,28 +90,28 @@ class Entity: public Node {
 
     public:
         Entity(
-            Rectangle rectangle, 
+            Vector2 position, 
             Vector2 velocity, 
             Vector2 acceleration, 
             Texture2D texture
         ):
-            Node(rectangle, texture), 
+            Node(position, texture), 
                 velocity(velocity), 
                 acceleration(acceleration) {};
 
         void update(std::list<std::shared_ptr<Node>> nodes) override {
-            
+
         }
 };
 
 class Player: public Entity {
     public:
         Player(
-            Rectangle rectangle, 
+            Vector2 position, 
             Vector2 velocity, 
             Vector2 acceleration, 
             Texture2D texture):
-                Entity(rectangle, velocity, acceleration, texture) {};
+                Entity(position, velocity, acceleration, texture) {};
 };
 
 int main() {
@@ -78,12 +122,12 @@ int main() {
     std::list<std::shared_ptr<Node>> nodes = {};
 
     nodes.push_back(std::make_shared<Platform>(
-        (Rectangle){100, 500, 100, 100}, 
+        (Vector2){100, 500}, 
         LoadTexture("assets/dirt_block.png"))
     );
 
     nodes.push_back(std::make_shared<Player>(
-        (Rectangle){100, 100, 32, 32},
+        (Vector2){100, 100},
         (Vector2){0, 0},
         (Vector2){0, 100},
         LoadTexture("assets/player.png"))

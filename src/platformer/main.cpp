227ebaf9aefd61a07baafde2b43067be1e7ce726
pepperjackdev <giacomodeflorio9093@gmail.com>
2026-01-04 22:06:44 +0100

    Refined structure and rewrote the collision handling system

--- src/platformer/main.cpp
+++ src/platformer/main.cpp
@@ -2,116 +2,68 @@
 #include <raymath.h>
 
 #include <memory>
-#include <cstdint>
-
 #include <list>
-#include <optional>
 
-enum class Collision: uint8_t {
-    RIGHTSIDE=0b11111011,
-    LEFTSIDE=0b11110111,
-    UPSIDE=0b01111111,
-    DOWNSIDE=0b10111111,
-};
-
-uint8_t inline CheckCollisionAlpha(Rectangle rec0, Rectangle rec1) {
-    return rec0.y + rec0.height <= rec1.y;
-};
-
-uint8_t inline CheckCollisionBeta(Rectangle rec0, Rectangle rec1) {
-    return rec0.y >= rec1.y + rec1.height;
-};
-
-uint8_t inline CheckCollisionGamma(Rectangle rec0, Rectangle rec1) {
-    return rec0.x + rec0.width >= rec1.x;
-}
-
-uint8_t inline CheckCollisionDelta(Rectangle rec0, Rectangle rec1) {
-    return rec0.x <= rec1.x + rec1.width;
-}
-
-uint8_t inline CheckCollision(Rectangle rec0, Rectangle rec1) {
-    return 0;
-}
-
-std::optional<Collision> CheckSideCollision(Rectangle rec0a, Rectangle rec0b, Rectangle rec1a, Rectangle rec1b) {
-    if (CheckCollisionAlpha(rec0b, rec1b) && CheckCollisionBeta(rec0b, rec1b) &&
-            !CheckCollisionGamma(rec0a, rec1a) && CheckCollisionGamma(rec0b, rec1b)) {
-        return std::optional(Collision::RIGHTSIDE);
-    } else if (CheckCollisionAlpha(rec0b, rec1b) && CheckCollisionBeta(rec0b, rec1b) &&
-            !CheckCollisionDelta(rec0a, rec1a) && CheckCollisionDelta(rec0b, rec1b)) {
-        return std::optional(Collision::LEFTSIDE);
-    } else if (CheckCollisionGamma(rec0b, rec1b) && CheckCollisionDelta(rec0b, rec1b) &&
-            !CheckCollisionAlpha(rec0a, rec1a) && CheckCollisionAlpha(rec0b, rec1b)) {
-        return std::optional(Collision::UPSIDE);
-    } else if (CheckCollisionGamma(rec0b, rec1b) && CheckCollisionDelta(rec0b, rec1b) &&
-            !CheckCollisionBeta(rec0a, rec1a) && CheckCollisionBeta(rec0b, rec1b)) {
-        return std::optional(Collision::DOWNSIDE);
-    } else {
-        std::nullopt;
-    }
-};
+#include <cstdint>
+#include <stdfloat>
+#include <limits>
 
 class Node {
-    protected:
+    private:
+        std::float32_t mass;
         Vector2 position;
+        Vector2 velocity;
+        Vector2 acceleration;
         Texture2D texture;
 
     public:
-        Node(
+        Node(uint32_t mass,
             Vector2 position, 
+            Vector2 velocity, 
+            Vector2 acceleration, 
             Texture2D texture): 
                 position(position), 
+                velocity(velocity), 
+                acceleration(acceleration), 
                 texture(texture) {};
+            
+        Rectangle getRectangle() {
+            return (Rectangle){
+                this->position.x,
+                this->position.y,
+                (float)this->texture.width,
+                (float)this->texture.height
+            };
+        };
 
-        virtual void update(std::list<std::shared_ptr<Node>> nodes) {}
+        Vector2 getMomentum() {
+            return Vector2Scale(this->velocity, this->mass);
+        }
+
+        void setMomentum(Vector2 momentum) {
+            this->velocity=Vector2Scale(momentum, 1.0f/this->mass);
+        }
+        
+        virtual void update(std::list<std::shared_ptr<Node>> nodes) {
+            // Updating position
+            this->position = Vector2Add(this->position, 
+                Vector2Scale(this->velocity, GetFrameTime()));
+
+            // Updating velocity
+            this->velocity = Vector2Add(this->velocity,
+                Vector2Scale(this->acceleration, GetFrameTime()));
+
+            // Collisions
+            for (std::shared_ptr<Node> node: nodes) {
+                if (CheckCollisionRecs(this->getRectangle(), node->getRectangle())) {
+                    
+                }
+            }
+        }
 
         void draw() {
-            DrawTextureV(
-                this->texture, 
-                this->position,
-                RAYWHITE
-            );
+            DrawTextureV(this->texture, this->position, RAYWHITE);
         }
-};
-
-class Platform: public Node {
-    public:
-        Platform(
-            Vector2 position, 
-            Texture2D texture):
-                Node(position, texture) {};
-};
-
-class Entity: public Node {
-    protected:
-        Vector2 velocity;
-        Vector2 acceleration;
-
-    public:
-        Entity(
-            Vector2 position, 
-            Vector2 velocity, 
-            Vector2 acceleration, 
-            Texture2D texture
-        ):
-            Node(position, texture), 
-                velocity(velocity), 
-                acceleration(acceleration) {};
-
-        void update(std::list<std::shared_ptr<Node>> nodes) override {
-
-        }
-};
-
-class Player: public Entity {
-    public:
-        Player(
-            Vector2 position, 
-            Vector2 velocity, 
-            Vector2 acceleration, 
-            Texture2D texture):
-                Entity(position, velocity, acceleration, texture) {};
 };
 
 int main() {
@@ -121,23 +73,27 @@ int main() {
 
     std::list<std::shared_ptr<Node>> nodes = {};
 
-    nodes.push_back(std::make_shared<Platform>(
-        (Vector2){100, 500}, 
-        LoadTexture("assets/dirt_block.png"))
-    );
+    nodes.push_back(std::make_shared<Node>(
+        std::numeric_limits<std::float32_t>::max(),
+        (Vector2){100, 800},
+        (Vector2){0, 0},
+        (Vector2){0, 0},
+        LoadTexture("assets/platform.png")
+    ));
 
-    nodes.push_back(std::make_shared<Player>(
+    nodes.push_back(std::make_shared<Node>(
+        1.0f,
         (Vector2){100, 100},
         (Vector2){0, 0},
         (Vector2){0, 100},
-        LoadTexture("assets/player.png"))
-    );
+        LoadTexture("assets/player.png")
+    ));
 
     while (!WindowShouldClose()) {
-        for (std::shared_ptr<Node> n: nodes) n->update(nodes);
+        for (std::shared_ptr<Node> node: nodes) node->update(nodes);
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            for (std::shared_ptr<Node> n: nodes) n->draw();
+            for (std::shared_ptr<Node> node: nodes) node->draw();
         EndDrawing();
     }
 
